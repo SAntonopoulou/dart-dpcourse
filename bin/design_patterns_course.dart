cf1c4141d6aa7c481e69743690b3b82fb36474cd
Sophia Antonopoulou <s.willowood@acg.edu>
2025-04-30 18:12:28 +0200

    Initial commit

---
+++ bin/design_patterns_course.dart
@@ -0,0 +1,5 @@
+import 'package:design_patterns_course/design_patterns_course.dart' as design_patterns_course;
+
+void main(List<String> arguments) {
+  print('Hello world: ${design_patterns_course.calculate()}!');
+}
